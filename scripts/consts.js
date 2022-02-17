ac93665bd3b9257804d649a899532231539bcbae
Artur <artur.jackowiak.tel@gmail.com>
2022-02-17 21:13:40 +0100

    Init files structure

---
+++ scripts/consts.js
@@ -0,0 +1,11 @@
+const formElement = document.getElementById("searchForm");
+const inputElement = document.getElementById("searchInput");
+const buttonElement = document.getElementById("searchSubmit");
+const dataContainerElement = document.getElementById("dataContainer");
+
+export const Elements = {
+    formElement: formElement,
+    inputElement: inputElement,
+    buttonElement: buttonElement,
+    dataContainerElement: dataContainerElement
+};
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+import { Elements } from "./const.js";
